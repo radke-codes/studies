71cb68155b296fb8def107a7dad7e85c97823f88
luccsj <oluccsj@gmail.com>
2026-04-04 18:51:14 -0300

    udp.v5

---
+++ web/HTML/advanced/04_forms/form_01/action_page.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Resultado do Formulário</title>
+    </head>
+    <body>
+        <h1> Resultado do Formulário </h1>
+        Primeiro Nome: <?php echo $_POST["pnome"]; ?>
+        <br>
+        Sobrenome: <?php echo $_POST["snome"]; ?>
+    </body>
+</html>
